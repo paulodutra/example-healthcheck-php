460b0d4dcbb363773389260cf720549ab4b74c79
Paulo Dutra <paulodutra.info@gmail.com>
2019-10-28 11:01:46 -0300

    foram comentados as funções, explicando sobre a sua finalidade

--- index.php
+++ index.php
@@ -1,46 +1,19 @@
 <?php
 
+    /**
+     * <b>$urls</b> Define em um array as urls que serão verificadas
+     */
     $urls = [
-        'http://aedes.cnm.org.br/',
-        'https://carteirinha.cnm.org.br/',
-        'http://www.crack.cnm.org.br/observatorio_crack/',
-        'https://credenciamento.cnm.org.br/',
-        'http://desastres.cnm.org.br/',
-        'http://doadores.cnm.org.br/',
-        'http://li.cnm.org.br/',
-        'http://lixoes.cnm.org.br/',
-        'http://politico.cnm.org.br/',
-        'https://credenciamento.cnm.org.br/',
-        'http://www2.conquistas.cnm.org.br/rp.php',
-        'http://agua.cnm.org.br/',
-        'http://municipios.cnm.org.br',
-        'http://areastecnicas.cnm.org.br/',
-        'http://barracnm.org.br/',
-        'http://dialogo.cnm.org.br/',
-        'http://doadores.cnm.org.br/',
-        'http://fia.cnm.org.br/',
-        'http://forumagua.cnm.org.br/',
-        'http://fotos.cnm.org.br',
-        'http://leidatransparencia.cnm.org.br/',
-        'http://lgotp.cnm.org.br/',
-        'http://lixoes.cnm.org.br/',
-        'http://memoria.cnm.org.br/',
-        'http://mobilizacao.cnm.org.br/',
-        'http://mobilizacaopermanente.cnm.org.br/',
-        'http://nordeste.cnm.org.br/',
-        'http://novosgestores.cnm.org.br/',
-        'http://ods.cnm.org.br/',
-        'http://panorama.cnm.org.br/',
-        'http://politico.cnm.org.br/',
-        'http://www.processoseletivo.cnm.org.br/',
-        'http://www.realidade.cnm.org.br/',
-        'http://rede.cnm.org.br/',
-        'http://reinserir.cnm.org.br/',
-        'http://relatorioanual.cnm.org.br/',
-        'http://restosapagar.cnm.org.br/',
-        'http://ssd.cnm.org.br/'
+        'url-1',
+        'url-2',
     ];
 
+    /**
+     * <b>urlTest<b/> Função responsável por realizar a requisição e verificar se o serviço
+     * esta ou não disponível de acordo com o status code retornado.
+     * @param $url
+     * @return true or false;
+     */
     function urlTest($url)
     {
         $timeout = 10; //10
@@ -51,11 +24,9 @@
         curl_setopt($curl, CURLOPT_TIMEOUT, $timeout);
         
         $request = curl_exec($curl);
-        //$response = trim(strip_tags($request));
+      
         $statusCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-        //var_dump($statusCode); //die();
-
-        //var_dump($statusCode); die();
+      
         if($statusCode == "200" || $statusCode == "302")
         {
             return true;
@@ -66,6 +37,11 @@
         curl_close($curl);
     }
 
+    /**
+     * <b>writeFile</b> Função responsável por escrever a resposta em um arquivo .txt 
+     * chamado "log-healthcheck.txt". 
+     * @param String $string
+     */
     function writeFile($string)
     {
         $file = fopen('/var/www/html/healthcheck/log-healthcheck.txt', "a+");
